d7b6142f94991a331d9d5edd9f28695f64148cd4
Gagan Kumar <gaganskumar3@gmail.com>
2019-02-06 16:40:35 -0500

    Changes made in component for button increment

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
